55149a9e62bfad3ad0e7996083aee8eadbb9d2b0
WuYuxi <845043146@qq.com>
2022-05-23 22:04:56 +0800

    add select

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -16,30 +16,52 @@ const data = reactive({
   },
 
   current_content: null,
+
+  options: [{
+          value: '选项1',
+          label: '层次数据'
+        }, {
+          value: '选项2',
+          label: '网络数据'
+        }, {
+          value: '选项3',
+          label: '多维数据'
+        }, {
+          value: '选项4',
+          label: '表格数据'
+        }, {
+          value: '选项5',
+          label: '时空数据'
+        }],
+
 });
 
 data.content = paper;
 
+ 
+
 const show_cards = computed(() => {
   if (data.filters.join("") === "") {
     //如果没有选中筛选器
-    return data.content;
+    return paper;
   } else {
     //如果有筛选
-    console.log("选中：", data.filters);//初始data.filters["","",""]
-    return data.content.filter((d) => {//开始筛选
+    console.log("选中：", data.filters); //初始data.filters["","",""]
+    return data.content.filter((d) => {
+      //开始筛选
       for (let i = 0; i < data.filters.length; i++) {
-        const filter = data.filters[i];//
+        const filter = data.filters[i]; //
         if (filter == "All") return true;
         else if (filter !== "") {
           console.log("filter:", filter);
-          if (typeof filter == "object") {//如果是object 就是
+          if (typeof filter == "object") {
+            //如果是object 就是
             //console.log("选中数据类型（是一个数组）");
             console.log("d.group", d.group);
-            if (d.group[2].includes("Text")) {//测试筛选数据类型为Text
+            if (d.group[2].includes("Text")) {
+              //测试筛选数据类型为Text
               return true;
-            }
-            else return false;
+            } else return false;
           } else {
             if (d.group.find((d) => d === filter) === undefined) {
               return false;
@@ -60,22 +82,28 @@ function Filter(filter_index, condition) {
   }
 }
 
+function SelectData(selectValue){
+  console.log(selectValue)
+}
+
 const detailView = ref(null);
 function ShowDetailWindow(content, card_rect) {
-  data.detail_window.x = card_rect.x - card_rect.width * 2;
-  data.detail_window.y = card_rect.y + card_rect.height / 2 ;
+  // data.detail_window.x = card_rect.x - card_rect.width * 2;
+  // data.detail_window.y = card_rect.y + card_rect.height / 2 ;
 
-  let w = window.innerWidth;
-  if (data.detail_window.x <= 0) {
-    data.detail_window.x = 0;
-  }
-  if (data.detail_window.x + 1000 >= w) {
-    data.detail_window.x = w - 1000;
-  }
+  // let w = window.innerWidth;
+  // if (data.detail_window.x <= 0) {
+  //   data.detail_window.x = 0;
+  // }
+  // if (data.detail_window.x + 1000 >= w) {
+  //   data.detail_window.x = w - 1000;
+  // }
 
   data.show_detail = true;
   data.current_content = content;
 }
+
+
 </script>
 
 <template>
@@ -86,19 +114,24 @@ function ShowDetailWindow(content, card_rect) {
         <div class="function-group">
           <div>MetaphorTypes</div>
           <el-tooltip class="item" effect="light" content="All" placement="top">
-          <el-button
-            color="rgb(72, 124, 198)"
-            type="primary"
-            @click="Filter(0, 'All')"
-          >
-            <!-- 第一组：隐喻类型：形式隐喻、交互隐喻 
+            <el-button
+              color="rgb(72, 124, 198)"
+              type="primary"
+              @click="Filter(0, 'All')"
+            >
+              <!-- 第一组：隐喻类型：形式隐喻、交互隐喻 
               选中All (0：是group第一个维度，"All":名字叫All)-->
 
-            <el-icon color="white"><aim /></el-icon>
-          </el-button>
+              <el-icon color="white"><aim /></el-icon>
+            </el-button>
           </el-tooltip>
 
-            <el-tooltip class="item" effect="light" content="Form" placement="top">
+          <el-tooltip
+            class="item"
+            effect="light"
+            content="Form"
+            placement="top"
+          >
             <el-button
               color="rgb(72, 124, 198)"
               type="primary"
@@ -109,15 +142,20 @@ function ShowDetailWindow(content, card_rect) {
               <el-icon color="white"><aim /></el-icon>
             </el-button>
           </el-tooltip>
-            <!-- 交互隐喻 Interaction  (0：是group第一个维度，"Interaction":形式隐喻-->
-           <el-tooltip class="item" effect="light" content="Interaction" placement="top">
-          <el-button
-            color="rgb(72, 124, 198)"
-            type="primary"
-            @click="Filter(0, 'Interaction')"
+          <!-- 交互隐喻 Interaction  (0：是group第一个维度，"Interaction":形式隐喻-->
+          <el-tooltip
+            class="item"
+            effect="light"
+            content="Interaction"
+            placement="top"
           >
-            <el-icon color="white"><aim /></el-icon>
-          </el-button>
+            <el-button
+              color="rgb(72, 124, 198)"
+              type="primary"
+              @click="Filter(0, 'Interaction')"
+            >
+              <el-icon color="white"><aim /></el-icon>
+            </el-button>
           </el-tooltip>
         </div>
 
@@ -125,71 +163,117 @@ function ShowDetailWindow(content, card_rect) {
         <div class="function-group">
           <div>MappingTypes</div>
           <!-- 自然景观Z -->
-          <el-tooltip class="item" effect="light" content="Natural Landscape" placement="top">
-          <el-button
-            color="rgb(72, 124, 198)"
-            type="primary"
-            @click="Filter(1, 'Z')"
+          <el-tooltip
+            class="item"
+            effect="light"
+            content="Natural Landscape"
+            placement="top"
           >
-            <el-icon color="white"><aim /></el-icon>
-          </el-button>
+            <el-button
+              color="rgb(72, 124, 198)"
+              type="primary"
+              @click="Filter(1, 'Z')"
+            >
+              <el-icon color="white"><aim /></el-icon>
+            </el-button>
           </el-tooltip>
 
           <!-- 人造实体R -->
-          <el-tooltip class="item" effect="light" content="Man-made" placement="top">
-          <el-button
-            color="rgb(72, 124, 198)"
-            type="primary"
-            @click="Filter(1, 'R')"
+          <el-tooltip
+            class="item"
+            effect="light"
+            content="Man-made"
+            placement="top"
           >
-            <el-icon color="white"><aim /></el-icon>
-          </el-button>
+            <el-button
+              color="rgb(72, 124, 198)"
+              type="primary"
+              @click="Filter(1, 'R')"
+            >
+              <el-icon color="white"><aim /></el-icon>
+            </el-button>
           </el-tooltip>
 
           <!-- 非实体F -->
-          <el-tooltip class="item" effect="light" content="Non-physical" placement="top">
-          <el-button
-            color="rgb(72, 124, 198)"
-            type="primary"
-            @click="Filter(1, 'F')"
+          <el-tooltip
+            class="item"
+            effect="light"
+            content="Non-physical"
+            placement="top"
           >
-            <el-icon color="white"><aim /></el-icon>
-          </el-button>
+            <el-button
+              color="rgb(72, 124, 198)"
+              type="primary"
+              @click="Filter(1, 'F')"
+            >
+              <el-icon color="white"><aim /></el-icon>
+            </el-button>
           </el-tooltip>
 
           <!-- 动作D -->
-          <el-tooltip class="item" effect="light" content="Action" placement="top">
-          <el-button
-            color="rgb(72, 124, 198)"
-            type="primary"
-            @click="Filter(1, 'D')"
+          <el-tooltip
+            class="item"
+            effect="light"
+            content="Action"
+            placement="top"
           >
-            <el-icon color="white"><aim /></el-icon>
-          </el-button>
+            <el-button
+              color="rgb(72, 124, 198)"
+              type="primary"
+              @click="Filter(1, 'D')"
+            >
+              <el-icon color="white"><aim /></el-icon>
+            </el-button>
           </el-tooltip>
 
           <!-- 可视化启发 -->
-          <el-tooltip class="item" effect="light" content="Visualization Inspiration" placement="top">
-          <el-button
-            color="rgb(72, 124, 198)"
-            type="primary"
-            @click="Filter(1, 'K')"
+          <el-tooltip
+            class="item"
+            effect="light"
+            content="Visualization Inspiration"
+            placement="top"
           >
-            <el-icon color="white"><aim /></el-icon>
-          </el-button>
+            <el-button
+              color="rgb(72, 124, 198)"
+              type="primary"
+              @click="Filter(1, 'K')"
+            >
+              <el-icon color="white"><aim /></el-icon>
+            </el-button>
           </el-tooltip>
-          
         </div>
 
+        <!-- 第3组：数据类型 ： 13种 -->
         <div class="function-group">
           <div>DataTypes</div>
-          <el-button
-            color="rgb(72, 124, 198)"
-            type="primary"
-            @click="Filter(2, 'Text')"
+          <!-- 文本数据Text -->
+          <el-tooltip
+            class="item"
+            effect="light"
+            content="Text"
+            placement="top"
           >
-            <el-icon color="white"><aim /></el-icon>
-          </el-button>
+            <el-button
+              color="rgb(72, 124, 198)"
+              type="primary"
+              @click="Filter(2, 'Text')"
+            >
+              <el-icon color="white"><aim /></el-icon>
+            </el-button>
+          </el-tooltip>
+
+          <el-select v-bind="value" placeholder="请选择">
+            <el-option
+              v-for="item in data.options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+              @click="SelectData(item.label)"
+            > 
+            <span style="float: left">{{ item.label }}</span>
+            </el-option>
+          </el-select>
+
         </div>
 
         <div class="function-group">
@@ -292,7 +376,9 @@ function ShowDetailWindow(content, card_rect) {
 }
 
 .detail-window {
-  position: absolute;
+  position: fixed;
+  left: 25vw;
+  height: 25vh;
   z-index: 10;
 }
 </style>
